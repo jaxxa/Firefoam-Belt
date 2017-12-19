32ed588e567a1a9a714d4a6bf67af97436c2043a
Joshua W <jaxxa90@gmail.com>
2017-12-20 07:05:22 +1100

    Using a New Comp

--- Source/FirefoamBelt/FirefoamBelt.cs
+++ Source/FirefoamBelt/FirefoamBelt.cs
@@ -8,22 +8,56 @@ using Verse;
 namespace FirefoamBelt
 {
 
-    public class FirefoamBelt : Apparel
+    public class CompFirefoamBelt : ThingComp
     {
 
-        public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
+        public CompProperties_FirefoamBelt Props
         {
+            get
+            {
+                return (CompProperties_FirefoamBelt)this.props;
+            }
+        }
+
+        public override void PostPreApplyDamage(DamageInfo dinfo, out bool absorbed)
+        {
+
+            absorbed = false;
+
             if (dinfo.Def == DamageDefOf.Burn || dinfo.Def == DamageDefOf.Flame)
             {
-                Log.Message("Fire! Fire!");
+                
+                //Log.Message("Fire! Fire!");
 
-                GenExplosion.DoExplosion(this.Wearer.Position, this.Wearer.Map, 5, DamageDefOf.Extinguish, this);
-                this.Destroy(DestroyMode.Vanish);
-                return true;
+                //GenExplosion.DoExplosion(this.Wearer.Position, this.Wearer.Map, 5, DamageDefOf.Extinguish, this);
+                //this.Destroy(DestroyMode.Vanish);
+                //return true;
+                var _Apparal = (Apparel)this.parent;
+
+                if (_Apparal != null)
+                {
+                    GenExplosion.DoExplosion(_Apparal.Wearer.Position, _Apparal.Wearer.Map, 5, DamageDefOf.Extinguish, _Apparal);
+                    absorbed = true;
+                }
+                else
+                {
+                    Log.Error("CompFirefoamBelt.PostPreApplyDamage failed to get Parent Apparal.");
+                }
+
             }
             //GenExplosion.DoExplosion(this.Wearer.Position, this.Wearer.Map, StatExtension.GetStatValue((Thing)this, StatDefOf.SmokepopBeltRadius, true), DamageDefOf.Smoke, (Thing)null, -1, (SoundDef)null, (ThingDef)null, (ThingDef)null, ThingDefOf.Gas_Smoke, 1f, 1, false, (ThingDef)null, 0.0f, 1, 0.0f, false);
-            return false;
+            //return false;
         }
     }
+
+    public class CompProperties_FirefoamBelt : CompProperties
+    {
+
+        public CompProperties_FirefoamBelt()
+        {
+            this.compClass = typeof(CompFirefoamBelt);
+        }
+    }
+
 }
 
