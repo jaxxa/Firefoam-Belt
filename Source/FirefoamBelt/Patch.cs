eee9b1d5990ec62ca0d987dc123bd2cf1d88a325
Joshua W <jaxxa90@gmail.com>
2018-09-10 07:42:09 +1000

    Initial update work

--- Source/FirefoamBelt/Patch.cs
+++ Source/FirefoamBelt/Patch.cs
@@ -63,68 +63,69 @@ namespace FirefoamBelt
 
         protected static Boolean Detonate_Prefix(ref CompExplosive __instance, Map map)
         {
-            Log.Message("Executing Detonate_Prefix");
-            //Log.Message("Map: " + (map != null).ToString());
-            if (!__instance.parent.SpawnedOrAnyParentSpawned)
+
+            Log.Message("Detonate");
+            if (__instance.parent.SpawnedOrAnyParentSpawned)
             {
-                Log.Message("Bailing out as Not SpawnedOrAnyParentSpawned");
-                return false; //Dont run origional
-            }
 
-            //Cache Position before Killing parent.
-            IntVec3 _Position = __instance.parent.PositionHeld;
-            Log.Message("PositionHeld Cached");
-            Log.Message(" Parent Pos: " + __instance.parent.Position.ToString() +
-                        " Parent Pos Held: " + __instance.parent.PositionHeld);
+                //Cache Position before Killing parent.
+                IntVec3 _Position = __instance.parent.PositionHeld;
 
-            CompProperties_Explosive props = __instance.Props;
-            float radius = __instance.ExplosiveRadius();
-            if ((double)props.explosiveExpandPerFuel > 0.0 && __instance.parent.GetComp<CompRefuelable>() != null)
-                __instance.parent.GetComp<CompRefuelable>().ConsumeFuel(__instance.parent.GetComp<CompRefuelable>().Fuel);
-            if ((double)props.destroyThingOnExplosionSize <= (double)radius && !__instance.parent.Destroyed)
-            {
-                __instance.destroyedThroughDetonation = true;
-                __instance.parent.Kill(new DamageInfo?(), (Hediff)null);
-            }
-
-            //this.EndWickSustainer();
-
-            //EndWickSustainer is Private so use Reflection to invoke the method.
-            MethodInfo _EndWickSustainer = typeof(CompExplosive).GetMethod("EndWickSustainer", BindingFlags.NonPublic | BindingFlags.Instance);
-            //Patch.LogNULL(_EndWickSustainer, "_EndWickSustainer", true);
-            if (_EndWickSustainer != null)
-            {
-                _EndWickSustainer.Invoke(__instance, null);
-            }
-
-            __instance.wickStarted = false;
-            if (map == null)
-            {
-                Log.Warning("Tried to detonate CompExplosive in a null map.");
-            }
-            else
-            {
-                Log.Message("About to Do Exposion, PositionHeld now returns Parent Position, not the same Pos Held Value as before the parent was killed, so use the cached value.");
-                Log.Message(" Parent Pos: " + __instance.parent.Position.ToString() +
-                            " Parent Pos Held: " + __instance.parent.PositionHeld);
-
-                if (props.explosionEffect != null)
+                CompProperties_Explosive props = __instance.Props;
+                float num = __instance.ExplosiveRadius();
+                if (props.explosiveExpandPerFuel > 0f && __instance.parent.GetComp<CompRefuelable>() != null)
                 {
-
-                    Effecter effecter = props.explosionEffect.Spawn();
-                    //effecter.Trigger(new TargetInfo(__instance.parent.PositionHeld, map, false), new TargetInfo(__instance.parent.PositionHeld, map, false));
-                    //Use the Cached Position
-                    effecter.Trigger(new TargetInfo(_Position, map, false), new TargetInfo(_Position, map, false));
-                    effecter.Cleanup();
+                    __instance.parent.GetComp<CompRefuelable>().ConsumeFuel(__instance.parent.GetComp<CompRefuelable>().Fuel);
+                }
+                if (props.destroyThingOnExplosionSize <= num && !__instance.parent.Destroyed)
+                {
+                    __instance.destroyedThroughDetonation = true;
+                    __instance.parent.Kill(null, null);
+                }
+                //__instance.EndWickSustainer();
+                //EndWickSustainer is Private so use Reflection to invoke the method.
+                MethodInfo _EndWickSustainer = typeof(CompExplosive).GetMethod("EndWickSustainer", BindingFlags.NonPublic | BindingFlags.Instance);
+                Patch.LogNULL(_EndWickSustainer, "_EndWickSustainer");
+                if (_EndWickSustainer != null)
+                {
+                    _EndWickSustainer.Invoke(__instance, null);
                 }
 
-                //Get the instigator unsing Reflection because it is private
-                FieldInfo _Instigator = typeof(CompExplosive).GetField("instigator", BindingFlags.NonPublic | BindingFlags.Instance);
-                //Patch.LogNULL(_Instigator, "_Instigator", true);
-                Thing _InstigatorValue = (Thing)_Instigator.GetValue(__instance);
+                __instance.wickStarted = false;
+                if (map == null)
+                {
+                    Log.Warning("Tried to detonate CompExplosive in a null map.", false);
+                }
+                else
+                {
+                    if (props.explosionEffect != null)
+                    {
+                        Effecter effecter = props.explosionEffect.Spawn();
+                       //effecter.Trigger(new TargetInfo(__instance.parent.PositionHeld, map, false), new TargetInfo(__instance.parent.PositionHeld, map, false));
+                        effecter.Trigger(new TargetInfo(_Position, map, false), new TargetInfo(_Position, map, false));
+                        effecter.Cleanup();
+                    }
+                    IntVec3 positionHeld = __instance.parent.PositionHeld;
+                    float radius = num;
+                    DamageDef explosiveDamageType = props.explosiveDamageType;
+                    
+                    //Get the instigator unsing Reflection because it is private
+                    FieldInfo _Instigator = typeof(CompExplosive).GetField("instigator", BindingFlags.NonPublic | BindingFlags.Instance);
+                    Patch.LogNULL(_Instigator, "_Instigator");
+                    Thing _InstigatorValue = (Thing)_Instigator.GetValue(__instance);
 
-                //GenExplosion.DoExplosion(this.parent.PositionHeld, map, radius, props.explosiveDamageType, this.instigator ?? (Thing)this.parent, props.damageAmountBase, props.explosionSound, (ThingDef)null, (ThingDef)null, props.postExplosionSpawnThingDef, props.postExplosionSpawnChance, props.postExplosionSpawnThingCount, props.applyDamageToExplosionCellsNeighbors, props.preExplosionSpawnThingDef, props.preExplosionSpawnChance, props.preExplosionSpawnThingCount, props.chanceToStartFire, props.dealMoreDamageAtCenter);
-                GenExplosion.DoExplosion(_Position, map, radius, props.explosiveDamageType, _InstigatorValue ?? (Thing)__instance.parent, props.damageAmountBase, props.explosionSound, (ThingDef)null, (ThingDef)null, props.postExplosionSpawnThingDef, props.postExplosionSpawnChance, props.postExplosionSpawnThingCount, props.applyDamageToExplosionCellsNeighbors, props.preExplosionSpawnThingDef, props.preExplosionSpawnChance, props.preExplosionSpawnThingCount, props.chanceToStartFire, props.dealMoreDamageAtCenter);
+                    Thing thing = _InstigatorValue ?? __instance.parent;
+
+                    int damageAmountBase = props.damageAmountBase;
+                    float armorPenetrationBase = props.armorPenetrationBase;
+                    SoundDef explosionSound = props.explosionSound;
+                    ThingDef postExplosionSpawnThingDef = props.postExplosionSpawnThingDef;
+                    float postExplosionSpawnChance = props.postExplosionSpawnChance;
+                    int postExplosionSpawnThingCount = props.postExplosionSpawnThingCount;
+                  //  GenExplosion.DoExplosion(positionHeld, map, radius, explosiveDamageType, thing, damageAmountBase, armorPenetrationBase, explosionSound, null, null, null, postExplosionSpawnThingDef, postExplosionSpawnChance, postExplosionSpawnThingCount, props.applyDamageToExplosionCellsNeighbors, props.preExplosionSpawnThingDef, props.preExplosionSpawnChance, props.preExplosionSpawnThingCount, props.chanceToStartFire, props.damageFalloff);
+
+                    GenExplosion.DoExplosion(_Position, map, radius, explosiveDamageType, thing, damageAmountBase, armorPenetrationBase, explosionSound, null, null, null, postExplosionSpawnThingDef, postExplosionSpawnChance, postExplosionSpawnThingCount, props.applyDamageToExplosionCellsNeighbors, props.preExplosionSpawnThingDef, props.preExplosionSpawnChance, props.preExplosionSpawnThingCount, props.chanceToStartFire, props.damageFalloff);
+                }
             }
 
             return false;
